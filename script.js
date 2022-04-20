7570996d44ed22ffec779345354d90ce9cb0948d
Ramon Nery <ramonnery2020@gmail.com>
2022-04-20 16:17:39 -0300

    Layout do tabuleiro finalizado.

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+function iniciaJogo() {
+    removeTagSeExistir('#escolhe-modo')
+    
+    // Removendo o atributo 'hidden' da div
+    document.getElementById('rodando-jogo').removeAttribute('hidden')
+}
+
+
+function removeTagSeExistir(nomeClasseOuId) {
+    if(!!document.querySelector(nomeClasseOuId)) {
+        document.querySelector(nomeClasseOuId).remove()
+    }
+}
+
+
+function criaOption(nomeValue, texto='') {
+    let option = document.createElement('option')
+    option.value = nomeValue
+    option.innerHTML = texto
+
+    return option
+}
+
 // Criando tags HTML
 let divDificuldade = document.createElement('div') // <div></div>
 divDificuldade.id = 'escolhe-dificuldade'
@@ -10,17 +33,9 @@ let criaSelect = document.createElement('select') //<select></select>
 criaSelect.id = 'dificuldade' //<select id='dificuldade'></select>
 criaSelect.name = 'dificuldade' //<select id='dificuldade' name='dificuldade'></select>
 
-let criaOption1 = document.createElement('option') // <option></option>
-criaOption1.value = 'facil'
-criaOption1.innerHTML = 'Fácil'
-
-let criaOption2 = document.createElement('option')
-criaOption2.value = 'medio'
-criaOption2.innerHTML = 'Médio'
-
-let criaOption3 = document.createElement('option')
-criaOption3.value = 'dificil'
-criaOption3.innerHTML = 'Difícil'
+let criaOption1 = criaOption('facil', 'Fácil')
+let criaOption2 = criaOption('medio', 'Médio')
+let criaOption3 = criaOption('dificik', 'Difícil')
 
 let button = document.createElement('button')
 button.classList.add('inicia-jogo')
@@ -28,6 +43,7 @@ button.textContent = 'Iniciar'
 button.addEventListener('click', iniciaJogo)
 
 let divEscolheModo = document.querySelector('#selecao')
+
 
 function addTags(value) {
     if(value === 'CPU') {
@@ -59,18 +75,6 @@ function addTags(value) {
         let divEscolheModo = document.querySelector('#escolhe-modo')
         divEscolheModo.appendChild(button)
 
-    }
-}
-
-
-function iniciaJogo() {
-    // removeTagSeExistir('#escolhe-modo')
-    console.log('O jogo começou...')
-}
-
-function removeTagSeExistir(nomeClasseOuId) {
-    if(!!document.querySelector(nomeClasseOuId)) {
-        document.querySelector(nomeClasseOuId).remove()
     }
 }
 
